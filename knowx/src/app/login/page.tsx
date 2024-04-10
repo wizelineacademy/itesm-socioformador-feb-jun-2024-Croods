99eb488e7b5cdf18b842f9ff79e477ca64bfae25
Sofía A01571120 <88906709+sofia-cantu@users.noreply.github.com>
2024-04-10 11:38:10 -0600

    Rutas
    ...

---
+++ knowx/src/app/login/page.tsx
@@ -0,0 +1,50 @@
+/* login.tsx */
+"use client";
+import Image from "next/image";
+import { useState } from "react";
+
+export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = async () => {
+    console.log("Login with:", email, password);
+    // Implement your login logic here, e.g., API call to your authentication service
+  };
+
+  return (
+    <main className="bg-backgroundLight">
+      <div className="mx-auto px-6 max-w-6xl h-screen text-gray-600 flex flex-wrap content-center justify-center">
+        <section className="fixed top-0 left-0 right-0 py-3 flex justify-center">
+          <Image
+            src="/Logo.svg"
+            alt="Next.js Logo"
+            width={100}
+            height={100}
+            priority
+          />
+        </section>
+        <div className="w-5/6 relative mt-20">
+          <input
+            type="email"
+            placeholder="Email"
+            className="bg-black mb-4 h-20 w-full rounded-lg text-white px-8 text-lg"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            className="bg-black mb-4 h-20 w-full rounded-lg text-white px-8 text-lg"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button
+            className="h-20 w-30 rounded-lg bg-blue-500 text-white px-4 text-lg"
+            onClick={handleLogin}
+          >
+            Login
+          </button>
+        </div>
+      </div>
+    </main>
+  );
+}
