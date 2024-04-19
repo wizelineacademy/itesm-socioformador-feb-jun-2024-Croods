c981c182a31a1dd8317a95e3116786f09ee400af
CaroGzzLeal <a01284948@tec.mx>
2024-04-19 12:17:15 -0600

    Dropdown menu

--- knowx/src/app/components/UserMenu.tsx
+++ knowx/src/app/components/UserMenu.tsx
@@ -1,5 +1,28 @@
 "use client"
 import { useSession, signOut } from "next-auth/react";
+import { Menu } from '@headlessui/react'
+import { ChevronDownIcon } from '@heroicons/react/20/solid'
+
+function MyDropdown() {
+  return (
+    <Menu>
+      <Menu.Button>
+        <ChevronDownIcon
+              className="-mr-1 ml-2 h-7 w-7 text-gray-600 dark:text-white"
+              aria-hidden="true"
+            />
+      </Menu.Button>
+      <Menu.Items className="absolute right-0 mt-32 w-full origin-top-right divide-y bg-[#4040401a] divide-gray-100 rounded-md shadow-lg ring-1 ring-black/5 focus:outline-none">
+        <Menu.Item>
+            <button onClick={() => signOut()} className="p-3 rounded-lg w-full font-semibold ">
+              Logout
+          </button>
+        </Menu.Item>
+      </Menu.Items>
+    </Menu>
+  )
+}
+
 
 export default function UserMenu({className}: {className: string}) {
   const { data: session } = useSession();
@@ -13,11 +36,9 @@ export default function UserMenu({className}: {className: string}) {
           className="w-10 h-10 rounded-full select-none"
           alt="User"
         />
+        <MyDropdown />
       </div>
 
-      <button onClick={() => signOut()} className="p-3 rounded-lg bg-[#4040401a] dark:bg-[#d9d9d91a]">
-        logout
-      </button>
     </div>
   );
 }
