f7770f7a262a73d2226846ca7b52765a3b7b855d
Diegogtzt03 <65473367+Diegogtz03@users.noreply.github.com>
2024-04-19 09:42:53 -0600

    Add authentication check in dashboard page

--- knowx/src/app/dashboard/page.tsx
+++ knowx/src/app/dashboard/page.tsx
@@ -1,8 +1,16 @@
 /* src/app/dashboard/page.tsx */
 
 import Image from "next/image";
+import { useSession } from "next-auth/react";
+import { redirect } from "next/navigation";
 
 export default function Home() {
+  const { data: session } = useSession();
+
+  if (!session) {
+    redirect('/auth');
+  }
+
   return (
     <main className="bg-backgroundLight">
       <div className="mx-auto px-6 max-w-6xl h-screen text-gray-600 flex flex-wrap content-center justify-center">
