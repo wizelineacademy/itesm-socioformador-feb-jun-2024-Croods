588f6dcf885be9e11752f7e090033b98a26973c2
Diegogtzt03 <65473367+Diegogtz03@users.noreply.github.com>
2024-04-22 18:30:40 -0600

    Update UserMenu component to use a blank user image and update alt text

--- knowx/src/app/components/UserMenu.tsx
+++ knowx/src/app/components/UserMenu.tsx
@@ -60,9 +60,9 @@ export default function UserMenu({className}: {className: string}) {
       <div className="flex relative items-center p-3 rounded-lg bg-[#4040401a] dark:bg-[#d9d9d91a]">
         <span className="hidden sm:block mr-2 text-lg font-semibold dark:text-white select-none">{session?.user?.name}</span>
         <img
-          src={session?.user?.image || "/user.svg"}
+          src={session?.user?.image || "/blankUser.png"}
           className="w-10 h-10 rounded-full select-none"
-          alt="User"
+          alt="User Image"
         />
         <MyDropdown />
       </div>
