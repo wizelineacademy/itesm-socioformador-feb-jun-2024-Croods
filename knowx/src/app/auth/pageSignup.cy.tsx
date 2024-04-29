81f73efc85d4210d357a2af3bcfc8f38d80b1591
Diegogtzt03 <65473367+Diegogtz03@users.noreply.github.com>
2024-04-28 18:45:42 -0600

    Add new files and update existing files for Cypress tests and configuration

---
+++ knowx/src/app/auth/pageSignup.cy.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import Signup from './page'
+
+describe('<Signup />', () => {
+  it('renders', () => {
+    // see: https://on.cypress.io/mounting-react
+    cy.mount(<Signup />)
+  })
+})
