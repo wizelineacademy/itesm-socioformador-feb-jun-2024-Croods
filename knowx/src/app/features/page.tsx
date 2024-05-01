ef81347c3f22328a52f6dfba7da06307eaed5fb1
Sofía A01571120 <88906709+sofia-cantu@users.noreply.github.com>
2024-05-01 00:31:38 -0600

    Arreglo y funcionalidad
    Features

--- knowx/src/app/features/page.tsx
+++ knowx/src/app/features/page.tsx
@@ -3,12 +3,21 @@
 import Image from "next/image";
 import { useState } from "react";
 
+
 export default function Features() {
 
     const [newFeature, setNewFeature] = useState<string>("");
+    const [featuresList, setFeaturesList] = useState<string[]>(["Feature 1", "Feature 2", "Feature 3", "Feature 4", "Feature 5", "Feature 6", "Feature 7"]); 
+    const addNewFeature = () => {
+        if (newFeature.trim() !== "") {
+            //featuresList.push(newFeature);
+            setFeaturesList([...featuresList, newFeature]);
+            setNewFeature(""); // Clear input after adding the feature
+        }
+    };
 
     return (
-        <main className="bg-backgroundLight">
+        <main className="flex bg-backgroundLight">
             <div className="mx-auto px-6 max-w-6xl h-screen text-gray-600 flex flex-wrap content-center justify-start flex-col">
                 <section className="relative top-0 left-0 right-0 py-3 flex justify-center">
                     <Image
@@ -25,54 +34,25 @@ export default function Features() {
                     <h1 className="text-5xl text-center text-black">Select your Favorites</h1>
                 </div>
 
-                <div className="flex w-full items-center justify-center p-3">
+                <div className="flex w-full items-center justify-center p-3 scroll-smooth">
                     <ul className="grid grid-cols-2 flex-wrap gap-8">
-                        
-                        <button className="text-2xl bg-black text-white rounded-xl py-4 px-8 text-wrap text-ellipsis overflow-hidden">
-                            Feature 1
-                        </button>
-
-                        <button className="text-2xl bg-black text-white rounded-xl py-4 px-8 text-wrap text-ellipsis overflow-hidden">
-                            Feature 1
-                        </button>
-
-                        <button className="text-2xl bg-black text-white rounded-xl py-4 px-8 text-wrap text-ellipsis overflow-hidden">
-                            Feature 1
-                        </button>
-
-                        <button className="text-2xl bg-black text-white rounded-xl py-4 px-8 text-wrap text-ellipsis overflow-hidden">
-                            Feature 1
-                        </button>
-
-                        <button className="text-2xl bg-black text-white rounded-xl py-4 px-8 text-wrap text-ellipsis overflow-hidden">
-                            Feature 1
-                        </button>
-
-                        <button className="text-2xl bg-black text-white rounded-xl py-4 px-8 text-wrap text-ellipsis overflow-hidden">
-                            Feature 1
-                        </button>
-
-                        <button className="text-2xl bg-black text-white rounded-xl py-4 px-8 text-wrap text-ellipsis overflow-hidden">
-                            Feature 1
-                        </button>
-
-                        <button className="text-2xl bg-black text-white rounded-xl py-4 px-8 text-wrap text-ellipsis overflow-hidden">
-                            Feature 1
-                        </button>
-
+                        {featuresList.map((feature, index) => (
+                            <button key={index} className="hover:bg-zinc-600 text-xl bg-black text-white rounded-xl py-3 px-8 text-wrap text-ellipsis overflow-hidden">
+                                {feature}
+                            </button>
+                        ))}
                     </ul>
                 </div>
 
                 <div className="flex mt-5 w-full items-center justify-center ">
                     <input
                         name=""
-                        className="bg-black rounded-lg text-white text-lg py-4 text-center"
+                        className="bg-black rounded-lg text-white text-base py-3 text-center"
                         onChange={(e) => setNewFeature(e.target.value)}
-                        value= "Add a new feature..."
+                        value={newFeature}
                     ></input>
-                    <button className="text-black text-4xl px-4"
-                        onClick={() => setNewFeature(newFeature)}>
-                            +
+                    <button className="text-black text-4xl pl-4 pr-1" onClick={addNewFeature}>
+                        +
                     </button>
                 </div>
                 
