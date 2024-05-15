c1b52bcd6c69b22c9e1a70063130750b29aef837
Diegogtzt03 <65473367+Diegogtz03@users.noreply.github.com>
2024-05-15 16:02:35 -0600

    fix: redeploy script fix

--- api/redeploy.sh
+++ api/redeploy.sh
@@ -1,12 +1,16 @@
-#!/bin/bash
+#!/bin/sh
 
 # cd into projects folder
-cd /root/itesm-socioformador-feb-jun-2024-Croods/api
+cd /root/itesm-socioformador-feb-jun-2024-Croods
+
+# Remove frontend folder
+rm -rf knowx
+
+# cd into projects folder
+cd api
 
 # git pull
-git pull && git reset origin/main --hard
-
-rm -rf knowx
+git pull
 
 # Spin docker containers down to prevent errors
 sudo docker compose -f docker-compose.yml down
