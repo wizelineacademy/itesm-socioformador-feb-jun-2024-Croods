2a9cfbfd4c311a008045c910c0cc9e20e8efc51a
Diegogtzt03 <65473367+Diegogtz03@users.noreply.github.com>
2024-05-21 10:36:58 -0600

    chore: Update database actions and schema for search log

--- knowx/db/schema.ts
+++ knowx/db/schema.ts
@@ -1,49 +1,19 @@
 import {
   pgTable,
   serial,
-  date,
   varchar,
   integer,
   timestamp,
   boolean,
   text,
-  primaryKey
+  primaryKey,
 } from "drizzle-orm/pg-core";
-import { drizzle } from 'drizzle-orm/neon-http';
-import { neon } from '@neondatabase/serverless';
-import type { AdapterAccount } from "next-auth/adapters"
+import { drizzle } from "drizzle-orm/neon-http";
+import { neon } from "@neondatabase/serverless";
+import type { AdapterAccount } from "next-auth/adapters";
 
 const pool = neon(process.env.DB_URL!);
-export const db = drizzle(pool)
-
-export const old_user = pgTable("old_user", {
-  id: serial("id").primaryKey(),
-  email: varchar("email", { length: 100 }).unique().notNull(),
-  region: varchar("region", { length: 20 }),
-  createdAt: date("createdAt"),
-});
-
-export const search_log = pgTable("search_log", {
-  id: serial("id").primaryKey(),
-  idUser: integer("idUser").references(() => old_user.id),
-  search: varchar("search", { length: 128 }),
-  timeOfSearch: timestamp("timeOfSearch", { withTimezone: true }),
-  feedback: boolean("feedback"),
-});
-
-export const login_log = pgTable("login_log", {
-  id: serial("id").primaryKey(),
-  idUser: integer("userId").references(() => old_user.id),
-  accessDate: timestamp("accessDate", { withTimezone: true }),
-});
-
-export const error_log = pgTable("error_log", {
-  id: serial("id").primaryKey(),
-  idUser: integer("userId").references(() => old_user.id),
-  description: text("description"),
-  errorTime: timestamp("errorTime", { withTimezone: true }),
-  origin: varchar("origin", { length: 100 }),
-});
+export const db = drizzle(pool);
 
 export const users = pgTable("user", {
   id: text("id")
@@ -53,8 +23,33 @@ export const users = pgTable("user", {
   email: text("email").notNull(),
   emailVerified: timestamp("emailVerified", { mode: "date" }),
   image: text("image"),
-})
- 
+});
+
+export const search_log = pgTable("search_log", {
+  id: serial("id").primaryKey(),
+  userId: text("userId").references(() => users.id),
+  search: varchar("search", { length: 128 }),
+  selectedTopics: text("selectedTopics"),
+  selectedCategories: text("selectedCategories"),
+  searchResults: text("searchResults"),
+  timeOfSearch: timestamp("timeOfSearch", { withTimezone: true }),
+  feedback: boolean("feedback"),
+});
+
+export const login_log = pgTable("login_log", {
+  id: serial("id").primaryKey(),
+  userId: text("userId").references(() => users.id),
+  accessDate: timestamp("accessDate", { withTimezone: true }),
+});
+
+export const error_log = pgTable("error_log", {
+  id: serial("id").primaryKey(),
+  userId: text("userId").references(() => users.id),
+  description: text("description"),
+  errorTime: timestamp("errorTime", { withTimezone: true }),
+  origin: varchar("origin", { length: 100 }),
+});
+
 export const accounts = pgTable(
   "account",
   {
@@ -77,16 +72,16 @@ export const accounts = pgTable(
       columns: [account.provider, account.providerAccountId],
     }),
   })
-)
- 
+);
+
 export const sessions = pgTable("session", {
   sessionToken: text("sessionToken").primaryKey(),
   userId: text("userId")
     .notNull()
     .references(() => users.id, { onDelete: "cascade" }),
   expires: timestamp("expires", { mode: "date" }).notNull(),
-})
- 
+});
+
 export const verificationTokens = pgTable(
   "verificationToken",
   {
@@ -97,9 +92,9 @@ export const verificationTokens = pgTable(
   (vt) => ({
     compoundKey: primaryKey({ columns: [vt.identifier, vt.token] }),
   })
-)
+);
 
-export type User = typeof old_user.$inferSelect;
+export type User = typeof users.$inferSelect;
 export type SearchLog = typeof search_log.$inferSelect;
 export type LoginLog = typeof login_log.$inferSelect;
 export type ErrorLog = typeof error_log.$inferSelect;
