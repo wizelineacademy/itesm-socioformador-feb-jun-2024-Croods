eee48cbb842c045a05b51496361f2163890af50d
CaroGzzLeal <a01284948@tec.mx>
2024-05-21 00:19:24 -0600

    All the data from the json shows in the cards

--- knowx/src/app/dashboard/compare/page.tsx
+++ knowx/src/app/dashboard/compare/page.tsx
@@ -1,59 +1,84 @@
 "use client";
 
+import { useEffect, useState } from "react";
 import Image from "next/image";
 import { useTheme } from "next-themes";
 import UserMenu from "@/app/components/UserMenu";
+import { Menu } from "@headlessui/react";
 
-const Card = ({ title, description, value1, value2, value3, value4 }: {title: string, description: string, value1: string, value2: string, value3: string, value4: string}) => (
-  <div className=" overflow-y-scroll bg-white dark:bg-gray-800 rounded-lg shadow-md p-10 max-w-sm ">
-    <h2 className="text-center text-2xl font-semibold mb-4">{title}</h2>
+interface CardData {
+  title: string;
+  description: string;
+  [key: string]: string;
+}
+
+const Card = ({ title, description, data, titles }: { title: string, description: string, data: { [key: string]: string }, titles: string[] }) => (
+  <div className="overflow-y-scroll bg-white dark:bg-gray-800 rounded-lg shadow-md p-10 max-w-sm ">
+    <Menu as="div" className="relative inline-block text-left w-full">
+      <Menu.Button className="inline-flex justify-center w-full rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500">
+        <h2 className="text-2xl font-semibold">{title}</h2>
+      </Menu.Button>
+      <Menu.Items className="origin-top-center absolute left-1/2 transform -translate-x-1/2 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
+        {titles.map((title, index) => (
+          <Menu.Item key={index}>
+            {({ active }) => (
+              <a
+                href="#"
+                className={`${
+                  active ? 'bg-gray-100' : ''
+                } block px-4 py-2 text-sm text-gray-700`}
+              >
+                {title}
+              </a>
+            )}
+          </Menu.Item>
+        ))}
+      </Menu.Items>
+    </Menu>
     <hr className="h-px my-8 bg-gray-200 border-2 dark:bg-gray-700" />
     <p className="text-center mb-4">{description}</p>
     <ul className="space-y-3">
-      <li className="flex items-center">
-      <span className="block w-5 h-5 bg-gray-600 rounded-full mr-3"></span> {value1}
-      </li>
-      <li className="flex items-center">
-      <span className="block w-5 h-5 bg-gray-600 rounded-full mr-3"></span> {value2}
-      </li>
-      <li className="flex items-center">
-      <span className="block w-5 h-5 bg-gray-600 rounded-full mr-3"></span> {value3}
-      </li>
-      <li className="flex items-center">
-      <span className="block w-5 h-5 bg-gray-600 rounded-full mr-3"></span> {value4}
-      </li>
+      {Object.entries(data).map(([key, value], index) => (
+        key !== 'Description' && (
+          <li key={index} className="flex items-center">
+            <span className="block w-5 h-5 bg-gray-600 rounded-full mr-3"></span> {`${key}: ${value}`}
+          </li>
+        )
+      ))}
     </ul>
   </div>
 );
 
 export default function Features() {
   const { resolvedTheme } = useTheme();
+  const [cardData, setCardData] = useState<CardData[]>([]);
+  const [titles, setTitles] = useState<string[]>([]);
 
-  const cardData = [
-    {
-      title: "Example 01",
-      description:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vel dui ornare, tristique massa a, euismod quam. Integer tempus quis ligula vitae fringilla. Ut tempor sed nisi ullamcorper finibus.",
-        value1: "1", 
-        value2: "2",
-        value3: "3", 
-        value4: "4"
-    },
-    {
-      title: "Example 02",
-      description:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vel dui ornare, tristique massa a, euismod quam. Integer tempus quis ligula vitae fringilla. Ut tempor sed nisi ullamcorper finibus.",
-        value1: "1", 
-        value2: "2",
-        value3: "3", 
-        value4: "4"
-    }
-  ];
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch('/data.json');
+      const data = await response.json();
+      const selectedSoftware = data.selectedSoftware;
+      const selectedtedData = Object.keys(data.finalAnswer) //returns an array of a given object's own enumerable property names. This suggests that data.finalAnswer is an object, and we want to create an array that contains all of its keys.
+        .filter(key => selectedSoftware.includes(key))
+        .map(key => ({
+          title: key,
+          description: data.finalAnswer[key].Description,
+          ...data.finalAnswer[key],
+        }));
+
+      const titles = Object.keys(data.finalAnswer);
+      setCardData(selectedtedData);
+      setTitles(titles);
+    };
+
+    fetchData();
+  }, []);
 
   return (
     <main className="flex bg-backgroundLight dark:bg-backgroundDark">
       <UserMenu className="absolute right-0 mr-3" />
-      <div className="mx-auto px-6 max-w-6xl h-screen text-gray-600 flex flex-wrap content-center justify-start flex-col">
+      <div className="mx-auto px-6 max-w-6xl h-full text-gray-600 flex content-center justify-start flex-col">
         <section className="relative top-0 left-0 right-0 py-3 flex justify-center">
           <Image
             className="relative top-0 left-0 right-0"
@@ -71,16 +96,14 @@ export default function Features() {
           </h1>
         </div>
 
-        <div className="w-full flex justify-around gap-x-10">
+        <div className="w-full grid grid-cols-3 justify-around gap-10">
           {cardData.map((card, index) => (
             <Card
               key={index}
               title={card.title}
               description={card.description}
-              value1={card.value1}
-              value2={card.value2}
-              value3={card.value3}
-              value4={card.value4}
+              data={Object.fromEntries(Object.entries(card).filter(([key]) => key !== 'title' && key !== 'description'))}
+              titles={titles}
             />
           ))}
         </div>
