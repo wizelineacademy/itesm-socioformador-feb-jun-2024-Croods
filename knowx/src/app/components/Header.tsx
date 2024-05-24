25508378fa422c9b249d35f35580ad097c9cf7d3
Diegogtzt03 <65473367+Diegogtz03@users.noreply.github.com>
2024-05-24 10:27:48 -0600

    Fixed header malfunctions

--- knowx/src/app/components/Header.tsx
+++ knowx/src/app/components/Header.tsx
@@ -4,41 +4,51 @@ import UserMenu from "./UserMenu";
 export default function Header({
   isDashboard = true,
   title,
+  userMenuShowBoth = false,
+  children,
 }: {
   isDashboard: boolean;
   title?: string;
+  userMenuShowBoth?: boolean;
+  children?: React.ReactNode;
 }) {
   return (
-    <header
-      className={
-        !title
-          ? "fixed top-0 left-0 right-0 py-3 flex justify-center items-center"
-          : "fixed top-0 left-0 right-0 py-8 flex justify-center items-center"
-      }
-    >
-      <Image
+    <div className="mx-auto h-screen text-gray-600 flex flex-wrap justify-center content-baseline">
+      <header
         className={
           !title
-            ? "dark:invert relative top-0 left-0 right-0"
-            : "dark:invert absolute top-0 left-0 my-5 ml-8"
+            ? "relative top-0 left-0 right-0 py-3 pb-20 flex justify-between items-center w-full h-fit"
+            : "top-0 left-0 right-0 pb-8 flex justify-between items-center w-full h-fit"
         }
-        src={"/Logo.svg"}
-        alt="KnowX Logo"
-        width={50}
-        height={50}
-        priority
-      />
+      >
+        {!title && <div className="invisible" />}
 
-      {title && (
-        <h1 className="text-5xl font-bold text-center text-backgroundDark dark:text-backgroundLight">
-          {title}
-        </h1>
-      )}
+        <Image
+          className={
+            !title
+              ? "dark:invert absolute top-0 left-0 right-0 mr-auto ml-auto pt-3"
+              : "dark:invert top-0 left-0 my-5 ml-8"
+          }
+          src={"/Logo.svg"}
+          alt="KnowX Logo"
+          width={50}
+          height={50}
+          priority
+        />
 
-      <UserMenu
-        className={!title ? "absolute right-0 mr-3" : "absolute right-0 mr-5"}
-        isDashboard={isDashboard}
-      />
-    </header>
+        {title && (
+          <h1 className="absolute left-0 right-0 text-5xl font-bold text-center text-backgroundDark dark:text-backgroundLight select-none">
+            {title}
+          </h1>
+        )}
+
+        <UserMenu
+          className={!title ? "right-0 mr-3" : "right-0 mr-5"}
+          isDashboard={isDashboard}
+          showBoth={userMenuShowBoth}
+        />
+      </header>
+      {children}
+    </div>
   );
 }
