7112379ea4f46b8aaf8c7743405123a45f30b2ae
CaroGzzLeal <a01284948@tec.mx>
2024-06-14 19:28:41 -0600

    Added e2e tests

---
+++ knowx/cypress/e2e/logout/logout.cy.ts
@@ -0,0 +1,21 @@
+//E2E test for the Sign out functionality - Carolina González A01284948
+describe("Sign out", () => {
+  it("passes", () => {
+    // Prepare the database
+    cy.request("http://localhost:3000/api/testing")
+    cy.login()
+    cy.wait(8000)
+    cy.visit("http://localhost:3000/")
+    cy.get("#dropdown-btn-icon").click()
+    cy.get("button").contains("Sign out").click()
+    cy.wait(8000)
+
+    // Check that the user is logged out
+    cy.get("button").should("contain", "Sign in with Email")
+    cy.wait(8000)
+
+    cy.visit("http://localhost:3000/")
+    cy.wait(8000)
+    cy.get("button").should("contain", "Sign in with Email")
+  })
+})
