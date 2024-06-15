b23759e96be0a4ab1c974b9e90db6fb08b939a7e
Sofía A01571120 <88906709+sofia-cantu@users.noreply.github.com>
2024-06-14 20:42:38 -0600

    E2E Funcional

--- knowx/cypress/e2e/viewHistory.cy.ts
+++ knowx/cypress/e2e/viewHistory.cy.ts
@@ -1,10 +1,29 @@
 // cypress/e2e/viewHistory.cy.ts
 // E2E tests (Ozner Leyva A01742377)
+
 describe("View History", () => {
-  it("displays search history correctly", () => {
-    cy.visit("/history")
-    cy.get(".history-list").should("be.visible")
-    cy.get(".history-list-item").first().click()
-    cy.get(".history-details").should("be.visible")
+  it("passes", () => {
+    cy.request("http://localhost:3000/api/testing")
+    cy.login()
+    cy.wait(8000)
+    cy.visit("http://localhost:3000/history")
+
+    // Verifica que la tabla esté visible
+    cy.get("table[aria-label='search history table']").should("be.visible")
+
+    // Verifica que haya al menos un elemento en el historial
+    cy.get("tbody > tr").should("have.length.greaterThan", 0)
+
+    // Verifica que la fecha y la hora se formateen correctamente
+    cy.get("tbody > tr")
+      .first()
+      .within(() => {
+        cy.get("td")
+          .eq(1)
+          .contains(/(\d{1,2}\/\d{1,2}\/\d{4})/)
+        cy.get("td")
+          .eq(2)
+          .contains(/(\d{1,2}:\d{2})/)
+      })
   })
 })
